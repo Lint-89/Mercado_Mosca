69d359cda1ab7e46c060abf1490f8786922d6712
guih_vb <guilherme11vilasboas@gmail.com>
2025-04-04 15:10:56 -0300

    FinalCommitV1.0

--- mercado_mosca/FoneHz.Designer.cs
+++ mercado_mosca/FoneHz.Designer.cs
@@ -158,7 +158,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(796, 450);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.button2);
